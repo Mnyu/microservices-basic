dbab93c834c3538cf75745ea4914aeb3d1d0a0e7
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-08-04 22:01:57 +0530

    added account microservice with a single basic endpoint to test registration with eureka discovery

---
+++ photo-app-account-management-service/src/main/java/com/learn/photoapp/account/PhotoAppAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learn.photoapp.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppAccountServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PhotoAppAccountServiceApplication.class, args);
+  }
+}
