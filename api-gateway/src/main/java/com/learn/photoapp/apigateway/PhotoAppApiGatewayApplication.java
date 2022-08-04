a6ae44dc52a1acf155597bc1e62cd2e759b7b58a
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-08-04 23:09:15 +0530

    added spring cloud api gateway - with route to users microservice.

---
+++ api-gateway/src/main/java/com/learn/photoapp/apigateway/PhotoAppApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.learn.photoapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiGatewayApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PhotoAppApiGatewayApplication.class, args);
+  }
+}
