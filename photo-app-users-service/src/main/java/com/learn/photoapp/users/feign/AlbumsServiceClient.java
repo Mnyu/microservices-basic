9131b3c76455d1675e5b9ec4b00fceab49020dca
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-08-22 23:01:22 +0530

    micro-services-communication : configure resilience4j circuit breaker.

--- photo-app-users-service/src/main/java/com/learn/photoapp/users/feign/AlbumsServiceClient.java
+++ photo-app-users-service/src/main/java/com/learn/photoapp/users/feign/AlbumsServiceClient.java
@@ -1,10 +1,12 @@
 package com.learn.photoapp.users.feign;
 
 import com.learn.photoapp.users.model.AlbumTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // This interface AlbumsServiceClient will be implemented by the framework, but we need to
@@ -14,6 +16,13 @@ import java.util.List;
 public interface AlbumsServiceClient {
 
   @GetMapping("/users/{userId}/albums")
+  @CircuitBreaker(name = "albums-ws-cb", fallbackMethod = "getAlbumsFallback")
   List<AlbumTO> getAlbums(@PathVariable("userId") String userId);
+
+  default List<AlbumTO> getAlbumsFallback(String userId, Throwable exception) {
+    System.out.println("Param : " + userId);
+    System.out.println("Exception took place : " + exception.getMessage());
+    return new ArrayList<>();
+  }
 
 }
